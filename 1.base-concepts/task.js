c7a93bafd6febd8df74bf0c42e482b1b02f349e4
Artem Mazetov <mazetoff@yandex.ru>
2022-12-25 14:09:29 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,11 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let vD = Math.sqrt(bb-4ac)
+  let [r1, r2a, r2b] = [-b/(2a), (-b+vD)/(2a), (-b-vD)/(2a)];
+
+  if (vD == 0) arr = r1;
+  if (vD > 0)  arr = r2a + ', ' r2b;
   return arr;
 }
 
